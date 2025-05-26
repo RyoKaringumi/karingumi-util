4e19a782399aed3da68b92a305a1ca333a363d23
RyoKaringumi <ryo@ezoe.net>
2025-05-26 18:21:29 +0900

    loggerの初期化方法を修正し、log4jsが存在しない場合はconsoleを使用するように変更。package.jsonの名前とバージョンを更新し、依存関係を整理。

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-import * as log4js from 'log4js';
+let logger: { info: (msg: string) => void } = console;
+try {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const log4js = require('log4js');
+	logger = log4js.getLogger();
+} catch (e) {
+	// log4jsが存在しない場合はconsoleを使用
+}
 
 export function shuffleArray<T>(arr:T[]): T[] {
 	for (let i = arr.length - 1; i > 0; i--) {
@@ -9,7 +16,6 @@ export function shuffleArray<T>(arr:T[]): T[] {
 }
 
 export async function* logAsyncGenerator<T>(arr: T[]): AsyncGenerator<T, void, unknown> {
-	const logger = log4js.getLogger();
 	for await (const item of arr) {
 		logger.info(`Processing item: ${item}`);
 		yield item;
